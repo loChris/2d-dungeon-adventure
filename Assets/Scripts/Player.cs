f3ba28e9ab878721d0b6c7fa9550f2f3365a9403
loChris <44450518+loChris@users.noreply.github.com>
2020-03-22 14:58:47 -0500

    added jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _jumpHeight = 10.0f;
+    [SerializeField] private bool _isGrounded = false;
+    [SerializeField] private bool _resetJumpCooldown = false;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -12,7 +17,43 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rigidbody2D.velocity = new Vector2(horizontalInput, _rigidbody2D.velocity.y);
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _rigidbody2D.velocity = new Vector2(moveHorizontal, _rigidbody2D.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Debug.Log("you jumped");
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpHeight);
+            StartCoroutine(ResetJumpCooldown());
+        }
+    }
+
+    bool IsGrounded ()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(
+            transform.position, 
+            Vector2.down, 
+            0.6f, 
+            _groundLayer.value
+            );
+        
+        if (hitInfo.collider != null)
+        {
+            if (_resetJumpCooldown == false)
+                return true;
+        }
+        return false;
+    }
+
+    IEnumerator ResetJumpCooldown()
+    {
+        _resetJumpCooldown = true;
+        yield return new WaitForSeconds(.1f);
+        _resetJumpCooldown = false;
     }
 }
