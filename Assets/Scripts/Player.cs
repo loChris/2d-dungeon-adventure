bb78aa01ff89417c847fab0964fb25e4357e77c6
loChris <44450518+loChris@users.noreply.github.com>
2020-03-22 14:07:01 -0500

    added player horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rigidbody2D.velocity = new Vector2(horizontalInput, _rigidbody2D.velocity.y);
+    }
+}
