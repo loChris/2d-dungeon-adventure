e9a38424a418974d8f98635e0219e65cc2dc2ab1
loChris <44450518+loChris@users.noreply.github.com>
2020-03-24 15:43:16 -0500

    moved my enemy code into one single enemy script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
+    protected Animator animator;
+    protected SpriteRenderer spriteRenderer;
+    protected Vector3 currentTarget;
     [SerializeField] protected Transform pointA, pointB;
     [SerializeField] protected int health;
     [SerializeField] protected int speed;
     [SerializeField] protected int gems;
 
-    public virtual void Attack()
+    public virtual void Start()
     {
-        Debug.Log("Base attack called");
+        Init();
     }
 
-    public abstract void Update();
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
+        {
+            return;
+        }
+        
+        Movement();
+    }
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+    
+    public virtual void Movement()
+    {
+        spriteRenderer.flipX = currentTarget == pointA.position;
+
+        if (transform.position == pointA.position)
+        {
+            animator.SetTrigger("Idle");
+            currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            animator.SetTrigger("Idle");
+            currentTarget = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            currentTarget, 
+            speed * Time.deltaTime
+        );
+    }
 }
--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -1,49 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Animator _animator;
-    private SpriteRenderer _spriteRenderer;
-    private Vector3 _currentTarget;
-
-    void Start()
+    public override void Start()
     {
-        _animator = GetComponentInChildren<Animator>();
-        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        base.Start();
         speed = 2;
-    }
-
-    public override void Update()
-    {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
-        {
-            return;
-        }
-        
-        Movement();
-    }
-
-    void Movement()
-    {
-        _spriteRenderer.flipX = _currentTarget == pointA.position;
-
-        if (transform.position == pointA.position)
-        {
-            _animator.SetTrigger("Idle");
-            _currentTarget = pointB.position;
-        }
-        else if (transform.position == pointB.position)
-        {
-            _animator.SetTrigger("Idle");
-            _currentTarget = pointA.position;
-        }
-
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            _currentTarget, 
-            speed * Time.deltaTime
-            );
     }
 }
