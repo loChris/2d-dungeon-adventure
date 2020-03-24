84a0345114eb7cf8af0a3f4b490ce1b7b3276b56
loChris <44450518+loChris@users.noreply.github.com>
2020-03-24 15:20:54 -0500

    added spider walk and idle behavior

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Vector3 _currentTarget;
+
     void Start()
     {
+        _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        speed = 2;
     }
 
     public override void Update()
     {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
+        {
+            return;
+        }
+        
+        Movement();
+    }
+
+    void Movement()
+    {
+        _spriteRenderer.flipX = _currentTarget == pointA.position;
+
+        if (transform.position == pointA.position)
+        {
+            _animator.SetTrigger("Idle");
+            _currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            _animator.SetTrigger("Idle");
+            _currentTarget = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            _currentTarget, 
+            speed * Time.deltaTime
+        );
     }
 }
